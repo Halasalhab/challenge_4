03dbfaf153c5dad84f56264899cc00ed43befd6a
Halasalhab <halasalhab0@gmail.com>
2020-11-05 11:52:17 +0200

    by work,it dosen't mean it work,now work

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5001;
+const app = express();
+app.use(express.static(__dirname + '/public'));
+app.listen(PORT);
+app.get('/', (req, res) => {
+    res.send({ working: true })
+})
 
-
+app.get('/home', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/public/index.html'))
+})
